d8768b687b655dac6b5d8788e13dd02d1ba3d1e9
Laurenz Fussenegger <fussenegger@multivative.com>
2025-02-06 17:35:25 +0100

    Create working pip install script

--- setup.py
+++ setup.py
@@ -1,30 +1,21 @@
-# setup.py
-from skbuild import setup
-from setuptools import Extension
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 import os
 
-project_root = os.path.abspath(os.path.dirname(__file__))
-lib_dir = os.path.join(project_root, "_skbuild", "win-amd64-3.11", "cmake-install", "lib")
-include_dir = os.path.join(project_root)
-
-# Define the extension module that will link against your C library.
-extensions = cythonize([
-   Extension(
-       "capture_wrapper",
-       sources=["capture_wrapper.pyx"],
-       include_dirs=[include_dir],
-       libraries=["accapture"],  # link with the capture library built by CMake
-       library_dirs=[lib_dir],
-   )
-])
+extensions = [
+    Extension(
+        "capture_wrapper",
+        ["capture_wrapper.pyx", "capture.c"],
+        include_dirs=[os.getcwd()],
+        libraries=["d3d11"]
+    )
+]
 
 setup(
     name="accapture",
-    ext_modules=extensions,
-    version="0.1",
+    ext_modules=cythonize(extensions),
+    version="0.2",
     description="A Python wrapper for capturing frames from Assetto Corsa textures",
     author="Laurenz Fussenegger",
     author_email="fussenegger@student.tugraz.at",
-    cmake_install_dir=".",
 )
